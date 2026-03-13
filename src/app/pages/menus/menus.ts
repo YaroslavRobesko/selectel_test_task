cf9ecb7427342d789074a91467dad8ad263403a1
YaroslavRobesko <aroslavrobesko@gmail.com>
2026-03-13 19:49:00 +0300

    feat: добавление инструментов для перехода по ссылкам без обновления страницы

--- src/app/pages/menus/menus.ts
+++ src/app/pages/menus/menus.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Menu } from './components/menu/menu';
 import { Item } from './types/types';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-menus',
-  imports: [Menu],
+  imports: [Menu, RouterLink],
   templateUrl: './menus.html',
   styleUrl: './menus.css',
 })
